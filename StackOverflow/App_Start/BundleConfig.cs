3a33ff2b4c8fba217acd934f15962f636af8567d
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-08 12:43:55 +0530

    register controller

--- StackOverflow/App_Start/BundleConfig.cs
+++ StackOverflow/App_Start/BundleConfig.cs
@@ -14,6 +14,14 @@ namespace StackOverflow.App_Start
             bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/jquery-3.7.1.js", "~/Scripts/umd/popper.js", "~/Scripts/bootstrap.js"));
             bundles.Add(new StyleBundle("~/Styles/bootstrap").Include("~/Content/bootstrap.css"));
             bundles.Add(new StyleBundle("~/Styles/site").Include("~/Content/Styles.css"));
+
+            //bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include(
+            //    "~/Scripts/bootstrap.min.js",
+            //    "~/Scripts/respond.min.js"));
+
+            //bundles.Add(new StyleBundle("~/Styles/bootstrap").Include(
+            //            "~/Content/bootstrap.min.css",
+            //            "~/Content/site.css"));
             BundleTable.EnableOptimizations = true;
         }
 
